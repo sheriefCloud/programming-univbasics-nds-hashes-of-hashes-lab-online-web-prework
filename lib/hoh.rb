6614b30b8a048e37d447f5f7c040d61b11170719
Sherief Farid <sherief@farid.cloud>
2020-01-03 04:03:36 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -53,7 +53,7 @@ def naming_system
             label: "Family",
             sub_category: {
               label: "Genus",
-              sub_category: = {
+              sub_category: {
                 label: "Species",
                 sub_category: nil}
             }
